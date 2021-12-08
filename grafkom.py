79bdbf884529f562f93a3ea35e362f5aaca7be59
Sri Adiningsi <77705048+ningsy123@users.noreply.github.com>
2021-12-08 20:22:30 +0800

    Update grafkom.py

--- grafkom.py
+++ grafkom.py
@@ -31,3 +31,16 @@ elif y1 == y2:
     grafik.plot(titik_A, titik_B)
     grafik.grid()
     grafik.show()
+
+ else:
+    titik_A = []
+    titik_B = []
+    m_gradiengaris = (y2 - y1) / (x2 - x1)
+    N = x2 - x1 + 1
+    for i in range (0,N,1):
+        nilai_y = m_gradiengaris * (x - x1) + y1
+        ya = round(nilai_y)
+        print('Garis yang di lewati oleh titik A da titik B yaitu ', x,',', ya)
+        titik_A.append(x)
+        titik_B.append(ya)
+        x+=1
