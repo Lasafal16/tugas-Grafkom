20833474ede3cef3569ac4941b2c188bbdb3a5b4
saryfah12 <95765105+saryfah12@users.noreply.github.com>
2021-12-08 23:59:23 +0800

    Update grafkom.py

--- grafkom.py
+++ grafkom.py
@@ -1,3 +1,11 @@
+"""Tugas Grafika Komputer
+Membuat garis dengan algoritma brute force 
+E1E120048 Safal
+E1E120082 Muhamad Ikhwan
+E1E120068 Febriansyah
+E1E120048 Sri Adiningsih
+E1E120094 Syarifah Nurhafni Ahmad """
+
 import numpy as np
 import matplotlib.pyplot as grafik
 
