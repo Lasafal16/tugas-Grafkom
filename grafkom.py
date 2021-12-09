4db5d5bff4ca4083da9b824214e2ef01cf42cffc
Lasafal16 <95747531+Lasafal16@users.noreply.github.com>
2021-12-09 13:43:57 +0800

    Update grafkom.py

--- grafkom.py
+++ grafkom.py
@@ -3,7 +3,7 @@ Membuat garis dengan algoritma brute force
 E1E120048 Safal
 E1E120082 Muhamad Ikhwan
 E1E120068 Febriansyah
-E1E120048 Sri Adiningsih
+E1E120020 Sri Adiningsih
 E1E120094 Syarifah Nurhafni Ahmad """
 
 import numpy as np
